74c76dc93beaa17f6415e1dca02748815195f798
nirmiller <nir.miller928@gmail.com>
2025-11-06 11:29:34 -0600

    Added more changes

--- grid.py
+++ grid.py
@@ -3,11 +3,14 @@ import numpy as np
 class Grid:
 
     def __init__(self, n_x, n_y, gridPointObject, random_init=True, random_seed=1, loadGrid=None):
-
+        
         np.random.seed(random_seed)
 
         self.gridPointObject = gridPointObject
-        
+
+        self.n_x = n_x
+        self.n_y = n_y
+
         if random_init:
             self.grid = self.initalize_grid()
             self.grid_history = []
@@ -16,17 +19,32 @@ class Grid:
             self.grid_history = loadGrid.grid_history
 
     def initalize_grid(self):
-
         spins = np.random.choice([-1, 1], size=(self.n_x, self.n_y))
 
         grid = np.empty((self.n_x, self.n_y), dtype=object)
         for i, j in np.ndindex(self.n_x, self.n_y):
             grid[i, j] = self.gridPointObject(i, j, spins[i, j])
+
         return grid
 
     def getPoint(self, point):
 
-        pass 
+        """
+        Here we define the getPoint method for the Grid class. This methods restrieves a requested point
+        based off its coordinates. Different grid topologies might implement this differently and will overide this method.
+        The default versian assumes a boudned square grid, meaning any points chosen outside the bound return None. 
+
+        Parameters
+            point (Classic Point Object): as a classical Ising model point with a spin
+        """
+
+        x_pos = point.x
+        y_pos = point.y
+
+        if 0 <= x_pos < self.n_x and 0 <= y_pos < self.n_y:
+            return self.grid[x_pos, y_pos]
+        else:
+            return None
 
     def update_grid(self, update_rule):
 
--- ising_model.py
+++ ising_model.py
@@ -61,37 +61,3 @@ class ClassicIsing:
         B_eff = self.effective_field(grid, i, j, J, h)
         return -s * B_eff
     
-
-class ClassicElectron:
-    def __init__(self, y, x, spin):
-
-        self.FermionSpin = 1/2
-        self.x = x
-        self.y = y
-
-        self.spin = spin
-
-
-        
-
-        self.G_factor = 2
-
-        # For electrons 
-        self.Bohr_Magneton = 9.274*(10**-24) 
-
-
-    def getSpin(self):
-        return self.spin*self.FermionSpin
-
-    def changeSpin(self, newSpin):
-        self.spin = newSpin
-
-    def calculateEnergy(self, effectiveMagneticField):
-
-        return self.spin*self.FermionSpin*self.G_factor*self.Bohr_Magneton*effectiveMagneticField
-        
-    def changeInEnergy(self, effectiveMagneticField):
-
-        newSpin = self.spin*(-1)
-
-        return (newSpin - self.spin)*self.FermionSpin*self.G_factor*self.Bohr_Magneton*effectiveMagneticField
